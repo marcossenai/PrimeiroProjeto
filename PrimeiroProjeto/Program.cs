80a0dc7f1f7050d298303509deb8c03b893a18c4
marcossenai <marcos.lacerda374@gmail.com>
2024-04-17 16:24:55 -0300

    Initial commit

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Desafios
+{
+    /*ATIVIDADE 1*/
+    /*class Program
+    {
+        static void Main(string[] args)
+        {
+            int Salario = 35;
+            int Horas = 8;
+
+            int multiplicacao = Salario * Horas;
+            Console.WriteLine(multiplicacao);
+        }
+
+    }*/
+
+
+    /*ATIVIDADE 2*/
+    /*class Program2
+    {
+        static void Main(string[] args)
+        {
+            int Bombom = 27;
+            int Caixas = 16;
+
+            int multiplicacao = Bombom * Caixas;
+            Console.WriteLine(multiplicacao);
+        }
+    }*/
+
+
+    /*ATIVIDADE 3*/
+    /*class Program3
+    {
+        static void Main(string[] args)
+        {
+            int numero1, numero2, soma;
+
+            Console.WriteLine("Digite o primeiro número inteiro:");
+            numero1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número inteiro:");
+            numero2 = int.Parse(Console.ReadLine());
+
+            soma = numero1 + numero2;
+
+            Console.WriteLine($"A soma de {numero1} e {numero2} é igual a {soma}.");
+        }
+    }*/
+
+
+
+    /*ATIVIDADE 4*/
+    /*class Program4
+    {
+        static void Main(string[] args)
+        {
+            int Garrafas = 300;
+            int Caixas = 20;
+            int Divisao = Garrafas/ Caixas;
+
+            Console.WriteLine(Divisao);
+        }
+    }*/
+
+
+
+    /*ATIVIDADE 5*/
+    /*class Program5
+    {
+        static void Main(string[] args)
+        {
+            // Definição dos valores dos serviços e do desconto
+            double valorInstalacao = 300.00;
+            double valorLimpeza = 150.00;
+            double valorManutencao = 499.99;
+            double desconto = 50.00;
+
+            // Calcula o total sem desconto
+            double totalSemDesconto = valorInstalacao + valorLimpeza + valorManutencao;
+
+            // Aplica o desconto
+            double totalComDesconto = totalSemDesconto - desconto;
+
+            // Exibe o total a ser pago pelo cliente
+            Console.WriteLine($"O total a ser pago pelo cliente, com desconto de R$ {desconto:0.00}, é de R$ {totalComDesconto:0.00}.");
+        }
+    }*/
+}
+
+
+
+
